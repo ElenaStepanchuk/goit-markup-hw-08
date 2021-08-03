deb98f33d5282b7f2c0aa17a0c4b257fb1e3441b
ElenaStepanchuk <podolyak365@gmail.com>
2021-08-04 01:29:30 +0300

    menu tel

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@
     const refs = {
       openMenuBtn: document.querySelector('[data-menu-open]'),
       closeMenuBtn: document.querySelector('[data-menu-close]'),
-      modal: document.querySelector('[mob-menu]'),
+      menu: document.querySelector('[mob-menu]'),
     };
   
     refs.openMenuBtn.addEventListener('click', toggleMenu);
